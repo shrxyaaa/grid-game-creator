c0d73607a90ed68841eebc6fd691eaa7f3e0773d
Yash Punia <puniayash@gmail.com>
2023-01-27 20:32:57 +0530

    initial finish gameplay page

--- components/GameArea.js
+++ components/GameArea.js
@@ -1,19 +1,37 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { heebo } from "utils/fonts";
+import { FaCheck, FaTimes } from "react-icons/fa";
+import AppContext from "./AppContext";
+
 
 function GameArea({ cardsData }) {
+  const context = useContext(AppContext);
   const [cardList, setCardList] = useState([]);
-  const [selectedCard, setSelectedCard] = useState(-1);
+  const [day, setDay] = useState(0);
+  const [selectedCardList, setSelectedCardList] = useState([]);
+  const [guess, setGuess] = useState("");
+  const [numMatchedPairs, setNumMatchedPairs] = useState(1)
 
-  const cardClickHandler = e => {
-	let cardId = e.target.getAttribute("cardId");
-	let elem = e.target;
-	let text = elem.firstChild;
-	text.classList.toggle("hidden")
-	if (selectedCard === -1) {
-		setSelectedCard(cardId)
-	}
-  }
+  const cardsLayoutStyle = `${heebo.className} p-8 w-24 h-24 xl:w-36 xl:h-36  grid place-items-center text-2xl border-4  rounded-xl transition cursor-pointer`;
+
+  const cardClickHandler = (e) => {
+    if (selectedCardList.length < 2 && e.target.getAttribute("cardid") != null) {
+      let elem = e.target;
+      let text = elem.firstChild;
+      if (text.classList.contains("hidden"))
+      {
+        let newList = selectedCardList;
+        newList.push(elem);
+        setSelectedCardList(newList);
+      }
+      else
+      {
+        selectedCardList.pop();
+      }
+      text.classList.toggle("hidden");
+      setDay(selectedCardList.length);
+    }
+  };
 
   const createCardList = () => {
     const list = [];
@@ -22,9 +40,10 @@ function GameArea({ cardsData }) {
       for (let j = 0; j < 2; j++) {
         let element = (
           <div
-            cardId={i}
+            key={`${i}${j}`}
+            cardid={i}
             onClick={cardClickHandler}
-            className={` ${heebo.className} p-8 w-36 grid place-items-center h-36 text-2xl border-4 bg-purple-100 border-purple-800 rounded-xl transition cursor-pointer hover:bg-purple-300`}
+            className={`${cardsLayoutStyle} bg-purple-100 border-purple-800 hover:bg-purple-300`}
           >
             <span className="hidden">{cardsData[i][j]}</span>
           </div>
@@ -42,15 +61,77 @@ function GameArea({ cardsData }) {
     setCardList(list);
   };
 
+  const checkCards = (checkCondition) => {
+    let cardId1 = selectedCardList[0].getAttribute("cardid");
+    let cardId2 = selectedCardList[1].getAttribute("cardid");
+    let equalOption = checkCondition == "equal" && cardId1 == cardId2;
+    let inequalOption = checkCondition == "inequal" && cardId1 != cardId2;
+
+    if (equalOption) {
+      setNumMatchedPairs(numMatchedPairs+1);
+      console.log(numMatchedPairs);
+      if (numMatchedPairs == cardsData.length) {
+        console.log("Game completed, now updating state")
+        context.setGameCompleted(true);
+      }
+    }
+
+    if (equalOption || inequalOption) setGuess("Correct Option!");
+    else setGuess("Incorrect Option!");
+
+    if (inequalOption && !equalOption) {
+      selectedCardList.forEach((elem) => {
+        elem.firstChild.classList.toggle("hidden");
+      });
+    } else {
+      selectedCardList.forEach((elem) => {
+        elem.removeAttribute("cardid");
+        elem.setAttribute("class", `${cardsLayoutStyle} bg-pink-100 border-pink-500`)
+      });
+    }
+
+    selectedCardList.pop();
+    selectedCardList.pop();
+    setDay(selectedCardList.length);
+    setTimeout(() => {
+      setGuess("");
+    }, 2000);
+  };
+
   useEffect(() => {
     createCardList();
   }, []);
 
   return (
-    <div className="grid grid-rows-4 grid-cols-4 gap-8 place-items-center text-center">
-      {cardList.map((card) => card)}
+    <div>
+      <div className="grid grid-rows-4 grid-cols-4 gap-8 place-items-center text-center">
+        {cardList.map((card) => card)}
+      </div>
+      <div className="w-3/5 mx-auto my-8 grid place-items-center">
+        {day == 2 && (
+          <div>
+            <p className={`${heebo.className} my-2 text-lg`}>
+              Are the two cards related?
+            </p>
+            <div className="flex gap-4">
+              <div
+                onClick={() => checkCards("equal")}
+                className="text-xl p-6 border-4 rounded-md w-fit cursor-pointer bg-green-100 border-green-400 hover:text-green-600"
+              >
+                <FaCheck />
+              </div>
+              <div
+                onClick={() => checkCards("inequal")}
+                className="text-xl p-6 border-4 rounded-md w-fit cursor-pointer bg-red-100 border-red-400 hover:text-red-600"
+              >
+                <FaTimes />
+              </div>
+            </div>
+          </div>
+        )}
+        {day < 2 && guess != "" && <div className="text-3xl">{guess}</div>}
+      </div>
     </div>
-	
   );
 }
 
--- pages/play/[gameId].js
+++ pages/play/[gameId].js
@@ -1,11 +1,14 @@
 import GameArea from "components/GameArea";
+import AppContext from "components/AppContext";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { FaCheck, FaTimes } from "react-icons/fa";
+import { useState, useContext, useEffect } from "react";
+import { montserrat, heebo } from "utils/fonts";
 
 function GameplayPage() {
   const router = useRouter();
+  const context = useContext(AppContext);
   const gameId = router.query.gameId;
+  const [timer, setTimer] = useState(0);
 
   const cardsData = [
     ["1+1", "2"],
@@ -18,24 +21,55 @@ function GameplayPage() {
     ["8+8", "16"],
   ];
 
+  const getTime = () => {
+    return `${Math.trunc(timer / 60)}:${timer % 60 < 10 ? "0" : ""}${
+      timer % 60
+    }`;
+  };
+
   useEffect(() => {
-    console.log("Hello");
-  }, []);
+    console.log(context.gameCompleted);
+    setTimeout(() => {
+      if (!context.gameCompleted) setTimer(timer + 1);
+    }, 1000);
+  }, [timer]);
 
   return (
-    <div>
-      {/* Timer at the top */}
-      <div className="w-1/2 mx-auto my-8">
+    <div className="grid grid-cols-[1fr_2fr_1fr] w-11/12 mx-auto my-4">
+      <div className="flex-col flex">
+        <p
+          className={`${montserrat.className} text-2xl text-pink-500 font-bold`}
+        >
+          Timer
+        </p>
+        <div className={`${heebo.className} text-4xl font-bold`}>
+          {getTime()}
+        </div>
+        {context.gameCompleted && (
+          <div className="my-auto ">
+            <p className={`${montserrat.className} text-3xl my-4`}>
+              Congratulations 🏆
+            </p>
+            <p className={`${heebo.className} text-xl`}>
+              You completed the game with{" "}
+              <span className="font-bold text-pink-500">{context.lives}</span>{" "}
+              lives left in just{" "}
+              <span className="font-bold text-pink-500">{getTime()}</span> time
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="mx-auto">
         <GameArea cardsData={cardsData} />
       </div>
-
-      <div className="w-3/5 mx-auto my-8 flex gap-4 justify-center">
-        Display these buttons when 2 cards are selected
-        <div className="text-xl p-6 border-4 rounded-md w-fit cursor-pointer bg-green-100 border-green-400 hover:text-green-600">
-          <FaCheck />
-        </div>
-        <div className="text-xl p-6 border-4 rounded-md w-fit cursor-pointer bg-red-100 border-red-400 hover:text-red-600">
-          <FaTimes />
+      <div className="flex-col flex items-end">
+        <p
+          className={`${montserrat.className} text-right text-2xl text-pink-500 font-bold`}
+        >
+          Lives Left
+        </p>
+        <div className={`${heebo.className} text-4xl font-bold`}>
+          {context.lives}
         </div>
       </div>
     </div>
